6bde9428cf882a28b876f372ba6ef3818200df58
Katrina Pritchard <pritckat@gmail.com>
2018-08-11 00:00:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(spot)
-  'spot'.to_i
+def input_to_index("spot")
+  "spot".to_i
   spot - 1
 end
 
